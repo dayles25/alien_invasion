820e284f2cafce16f24d8ae7e581da5c0e0623f1
dayles25 <m250252@usna.edu>
2022-10-18 08:44:34 -0400

    initial game setup

---
+++ game.py
@@ -0,0 +1,29 @@
+import pygame
+import sys
+
+class AlienInvasion:
+    '''Overall class to manage game assets and behavior'''
+
+    def __init__(self):
+        '''Initialize game and create game resources'''
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+        self.bg_color = (18, 30, 199)
+
+    def run_game(self):
+        '''Start the main loop for the game'''
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.screen.fill(self.bg_color)
+
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
