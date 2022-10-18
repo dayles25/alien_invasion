1524ff0625287394d7a4fa94ec5e233bf05ffcc0
dayles25 <m250252@usna.edu>
2022-10-18 09:07:29 -0400

    ship created

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
 import pygame
 import sys
+from settings import Settings
+from ship import Ship
+
 
 class AlienInvasion:
     '''Overall class to manage game assets and behavior'''
@@ -7,8 +10,9 @@ class AlienInvasion:
     def __init__(self):
         '''Initialize game and create game resources'''
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
         self.bg_color = (18, 30, 199)
@@ -20,7 +24,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             pygame.display.flip()
 
