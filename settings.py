ef368710c2fc42728f36ad96b88b2a04acd4ba92
dayles25 <m250252@usna.edu>
2022-10-25 08:38:46 -0400

    left and right

--- settings.py
+++ settings.py
@@ -6,3 +6,4 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 600
         self.bg_color = (18, 30, 199)
+        self.ship_speed = 1.5
--- ship.py
+++ ship.py
@@ -14,11 +14,14 @@ class Ship:
         self.rect.midbottom = self.screen_rect.midbottom
 
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
         '''update ship's position based on the movement flag'''
         if self.moving_right:
             self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
 
     def blitme(self):
         '''draw the ship at its current location'''
