5ee535e74ce02f7fa0b58542689d30886a76283a
dayles25 <m250252@usna.edu>
2022-10-25 09:27:06 -0400

    hw files

---
+++ game_character.py
@@ -0,0 +1,18 @@
+import pygame
+
+class Ship:
+    '''A class to manage the ship'''
+
+    def __init__(self, ai_game):
+        '''initialize the ship and sets its starting position'''
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        self.image = pygame.image.load('images/coolspaceship.png')
+        self.image = pygame.transform.scale(self.image, (100, 150))
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        '''draw the ship at its current location'''
+        self.screen.blit(self.image, self.rect)
